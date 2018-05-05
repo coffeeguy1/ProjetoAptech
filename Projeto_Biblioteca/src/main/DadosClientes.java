7e34349ade1078e3701a363f5b6f9d8d3e167252
Rafael <Rafael@192.168.15.12>
2018-05-05 03:08:59 -0300

    Esta logando

--- Projeto_Biblioteca/src/main/DadosClientes.java
+++ Projeto_Biblioteca/src/main/DadosClientes.java
@@ -72,6 +72,7 @@ public class DadosClientes extends javax.swing.JFrame {
         jTextField11 = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         jTextField12 = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Dados do Cliente");
@@ -314,6 +315,19 @@ public class DadosClientes extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Marcar Retirada", jPanel3);
 
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 981, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 406, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Marcar Devolução", jPanel2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -519,6 +533,7 @@ public class DadosClientes extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTextField jTextField1;
